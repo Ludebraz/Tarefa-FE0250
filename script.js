c00feb9ac2f88a6bd5d50fa531f49c6606c735a0
Lude Braz <Ludebraz@gmail.com>
2024-12-08 23:01:51 -0300

    Tarefa FE55051TAREFA

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ function isPalindrome(frase){
     return true
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+let arrayT2 = [8, 6, 4 , 1, 59, 7, 9, 89, 5, 3]
+
+function arrayMaxMin(arrayT2){
+
+    let arrayRetorno = [0, 0]
+    let aux = 0
+    let n = arrayT2.length-1
+        for(let j = 0; j < n; j++){
+            for(let i = 0; i < n -j; i++){
+                if(arrayT2[i] > arrayT2[i+1]){
+                aux = arrayT2[i]
+                arrayT2[i] = arrayT2[i+1]
+                arrayT2[i+1] = aux       
+                }
+                
+            }
+
+        }
+    arrayRetorno[0] = arrayT2[0]
+    arrayRetorno[1] = arrayT2[9]
+    return arrayRetorno
 }
